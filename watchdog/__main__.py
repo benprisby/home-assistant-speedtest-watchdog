292359d9ba9af4d0ae128165b42db977d2ed9ccc
Ben Prisby <ben@benprisby.com>
2022-10-27 07:38:57 -0400

    Initial commit.

---
+++ watchdog/__main__.py
@@ -0,0 +1,58 @@
+import argparse
+import json
+import logging
+import os
+import signal
+import sys
+import threading
+import time
+import typing
+
+import watchdog.integration_reloader
+import watchdog.sensor_monitor
+
+logger = logging.getLogger(__name__)
+stop_signal = threading.Event()
+
+
+def signal_handler(signal: int, frame: object) -> None:
+    logger.info('Got signal %d', signal)
+    stop_signal.set()
+
+
+def main() -> None:
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s [%(module)s:%(lineno)d]',
+                        datefmt='%Y-%m-%dT%M:%H:%SZ',
+                        level=logging.DEBUG)
+    logging.Formatter.converter = time.gmtime  # UTC
+
+    parser = argparse.ArgumentParser(description='Reload the Home Assistant Speedtest integration when it dies')
+    parser.add_argument('-c', '--config', metavar='<path>', help='Configuration file path', required=True)
+    args = parser.parse_args()
+
+    config = {}
+    try:
+        with open(args.config, 'r', encoding='utf-8') as config_file:
+            config = json.load(config_file)
+            logger.debug('Loaded configuration file: %s', os.path.realpath(config_file.name))
+    except json.JSONDecodeError:
+        sys.exit('Invalid configuration file format')
+    except OSError:
+        sys.exit('Failed to open configuration file')
+
+    home_assistant_config: dict[str, typing.Any] = config['home_assistant']
+
+    reloader = watchdog.integration_reloader.IntegrationReloader(
+        **{key: value for key, value in home_assistant_config.items() if key != 'sensor_name'})
+    monitor = watchdog.sensor_monitor.SensorMonitor(reloader,
+                                                    **{'mqtt_' + key: value for key, value in config['mqtt'].items()},
+                                                    sensor_name=home_assistant_config['sensor_name'])
+
+    monitor.run(stop_signal)
+
+
+if __name__ == '__main__':
+    main()
---
+++ watchdog/integration_reloader.py
@@ -0,0 +1,30 @@
+import logging
+import requests
+
+logger = logging.getLogger(__name__)
+
+
+class IntegrationReloader:
+
+    def __init__(self,
+                 address: str,
+                 port: int,
+                 api_token: str,
+                 entry_id: str,
+                 https: bool = True,
+                 verify_certificate: bool = False) -> None:
+        scheme = 'https' if https else 'http'
+        self._url = f'{scheme}://{address}:{port}/api/services/homeassistant/reload_config_entry'
+        self._headers = {'Authorization': f'Bearer {api_token}'}
+        self._payload = {"entry_id": entry_id}
+        self._verify = verify_certificate
+
+        logger.debug('Initialized reloader for server at: %s', address)
+
+    def reload(self) -> bool:
+        try:
+            response = requests.post(self._url, headers=self._headers, json=self._payload, verify=self._verify)
+            response.raise_for_status()
+            return True
+        except requests.exceptions.HTTPError:
+            return False
