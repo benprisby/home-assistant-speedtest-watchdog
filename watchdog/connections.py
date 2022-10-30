e8bcbc5e2edbda438e5a1140a6dd563e0cc3dce3
Ben Prisby <ben@benprisby.com>
2022-10-30 19:07:57 -0400

    Support Python 3.9+.

--- watchdog/connections.py
+++ watchdog/connections.py
@@ -3,7 +3,7 @@
 import dataclasses
 
 
-@dataclasses.dataclass(kw_only=True)
+@dataclasses.dataclass()
 class BaseConnection:
     address: str
     port: int
@@ -12,18 +12,18 @@ class BaseConnection:
         return bool(self.address) and self.port > 0 and self.port <= 65535
 
 
-@dataclasses.dataclass(kw_only=True)
+@dataclasses.dataclass()
 class HomeAssistantConnection(BaseConnection):
     port: int = 8123
-    api_token: str
     https: bool = True
     verify_certificate: bool = True
+    api_token: str = ''  # BDP: Must have default argument until bumping to Python 3.10, where kw_only is available
 
     def is_valid(self) -> bool:
         return BaseConnection.is_valid(self) and bool(self.api_token)
 
 
-@dataclasses.dataclass(kw_only=True)
+@dataclasses.dataclass()
 class MqttConnection(BaseConnection):
     port: int = 1883
     username: str = ''
